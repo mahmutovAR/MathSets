4c37ac05e969c1c83789482d2386bab3e3ef880f
mahmutovAR <armax045@gmail.com>
2022-06-25 22:24:34 +0400

    version 1

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
 ﻿# MathSets
 
 This script gets math sets and some number and checks
-if given number belongs to some subrange from the given sets intersection.
+if given number belongs to some subrange from intersection of the initial sets.
 
-## Initial math sets are given as dictionary {key: value}:
-* Key is name of sub set
-* Value is sub set, is given as list, for example:
+## Initial math sets are given as dictionary {'name (if needed)': ['sub set']}:
+* The sub set is given as `list`, for example:
   * `[['-inf', -10], [10, '+inf']]`
   * `[['-inf', -41], [-18, 24], [51, 62], [103, '+inf']]`
   * `[[-89, -61], [61, 72]]`
 
 ### Task 1
-* return the initial number, if it belongs to some subrange from the math sets intersection
-* return the nearest endpoint(s) to the initial number otherwise
+* script returns the initial number, if it belongs to some subrange from the math sets intersection
+* script returns the nearest endpoint(s) to the initial number otherwise
 
 ### Task 2
-* return the subrange from the math sets intersection, if initial number belongs to this subrange
-* return the nearest range(s) from the math sets intersection otherwise
+* script returns the subrange from the math sets intersection, if initial number belongs to it
+* script returns the nearest range(s) from the math sets intersection otherwise
   
 ## Script runs on Python 3.9
--- math_sets.py
+++ math_sets.py
@@ -1,73 +1,20 @@
-def get_ini_data() -> dict and float:
-    """Returns initial math sets and number.
-    Types of math sets:
-        -inf                                                +inf
-        *------------------------------------------------------*
-        *-----------------------| |----------------------------*
-        *--------------------------------------------|
-                |----------------------------------------------*
-        *------------|  |----------|
-                  |--------------|      |----------------------*
-        *------------|    |----|       |--|     |--------------*
-                    |-------------------------------|
-            |------|        |------|    |-|       |-------|
-        All nine types are given in "all_sets"."""
+def format_sets(ini_math_sets: list) -> set and list:
+    """Returns set of all unique endpoints and list with formatted math sets:
+    '-inf' changed to min endpoint, '+inf' changed to max endpoint."""
+    all_endpoints = [end_point
+                     for sets_list in ini_math_sets
+                     for sub_set in sets_list
+                     for end_point in sub_set
+                     if not isinstance(end_point, str)]
 
-    sets_1 = {'set_1': [[-10.3, 10.3], [18.7, '+inf']],
-              'set_2': [['-inf', -8.2], [8.6, '+inf']],
-              'set_3': [['-inf', -3.7]]}
-    sets_2 = {'set_1': [['-inf', 10], [18, 26]],
-              'set_2': [['-inf', -8], [8.1, 22]],
-              'set_3': [['-inf', -3.74], [10, 30]]}
-    sets_3 = {'set_1': [[-10, 10], [18, '+inf']],
-              'set_2': [['-inf', -8], [8, '+inf']],
-              'set_3': [['-inf', -3], [19, '+inf']]}
-    sets_4 = {'set_1': [[-10, 10], [18, 22]],
-              'set_2': [[-16, -8], [8, 40]],
-              'set_3': [[-10, 23]]}
-    sets_5 = {'set_1': [[-5, 16.08]],
-              'set_2': [[16.02, 20]]}
-    all_sets = {
-        'type_1': [['-inf', '+inf']],
-        'type_2': [['-inf', -10], [10, '+inf']],
-        'type_3': [['-inf', 99]],
-        'type_4': [[-98, '+inf']],
-        'type_5': [['-inf', -32], [-17, 22]],
-        'type_6': [['-inf', -41], [-18, 24], [51, 62], [103, '+inf']],
-        'type_7': [[-89, -61], [-24, '+inf']],
-        'type_8': [[-77, 61]],
-        'type_9': [[-89, -61], [-43, -12], [10, 27], [61, 72]]
-    }
+    all_endpoints = set(all_endpoints)
 
-    x_1 = 6
-    x_2 = -16.51
-    x_4 = 14
-    x_5 = 16.06
-    x_6 = -1
+    min_value = min(all_endpoints)
+    max_value = max(all_endpoints)
 
-    return sets_2, -10
-    # return sets_3, 5.5
-    # return all_sets, -39
+    sub_sets_formatted = list()
 
-
-def format_sets(input_dict: dict) -> list and dict:
-    """Returns list of all endpoints and dictionary with formatted sets:
-    '-inf' changed to min value, '+inf' changed to max value."""
-    all_ranges = set()
-
-    for ranges in input_dict.values():
-        add_data = [end_point
-                    for sub_set in ranges
-                    for end_point in sub_set
-                    if not isinstance(end_point, str)]
-        all_ranges.update(add_data)
-
-    min_value = min(all_ranges)
-    max_value = max(all_ranges)
-
-    ini_sets = dict()
-
-    for set_name, ranges in input_dict.items():
+    for ranges in ini_math_sets:
         temp_list = list()
         for sub_range in ranges:
             if sub_range == ['-inf', '+inf']:
@@ -80,68 +27,56 @@ def format_sets(input_dict: dict) -> list and dict:
                     temp_list.append([sub_range[0], max_value])
             else:
                 temp_list.append(sub_range)
-        ini_sets[set_name] = temp_list
+        sub_sets_formatted.append(temp_list)
 
-    return all_ranges, ini_sets
+    return all_endpoints, sub_sets_formatted
 
 
-def get_ranges_for_check(input_list: set) -> list:
-    """Converts endpoints list to list of ranges."""
-    input_list = sorted(input_list)
-    output_list = [[input_list[point - 1], input_list[point]]
-                   for point in range(1, len(input_list))]
+def get_ranges_for_check(input_data: set) -> list:
+    """Converts endpoints set to list of ranges."""
+    input_data = sorted(input_data)
+    output_list = [[input_data[point - 1], input_data[point]]
+                   for point in range(1, len(input_data))]
     return output_list
 
 
 def check_ranges(ini_range: list, checking_range: list) -> list:
-    """Checks two ranges for overlap."""
-    intersection_ranges = list()
-    for sub_set in ini_range:
-        for sub_range in checking_range:
-            if sub_set[0] <= sub_range[0] and sub_set[1] >= sub_range[1]:
-                intersection_ranges.append(sub_range)
+    """Checks if the intersection sub range belongs to initial math sets."""
+    intersection_ranges = [checking_sub_range
+                           for ini_sub_set in ini_range
+                           for checking_sub_range in checking_range
+                           if ini_sub_set[0] <= checking_sub_range[0] and ini_sub_set[1] >= checking_sub_range[1]]
+
     return intersection_ranges
 
 
-def common_inf_range(ini_dict: dict, intersection: list, min_value: int or float, max_value: int or float) -> list:
-    """Checks initial math sets for the next conditions:
+def common_inf_range(ini_sets: list, intersection: list, min_value: int or float, max_value: int or float) -> list:
+    """Checks initial given math sets for the next cases:
         if all sets start from '-inf', then range ['-inf', min_value] will be added to the intersection list;
         if all sets end with '+inf', then range [max_value, '+inf'] will be added to the intersection list."""
-    if all(sub_set[0][0] == '-inf' for sub_set in ini_dict.values()):
+    if all(sub_set[0][0] == '-inf' for sub_set in ini_sets):
         intersection.insert(0, ['-inf', min_value])
-    if all(sub_set[-1][1] == '+inf' for sub_set in ini_dict.values()):
+    if all(sub_set[-1][1] == '+inf' for sub_set in ini_sets):
         intersection.append([max_value, '+inf'])
+
     return intersection
 
 
-def print_result(intersection: list) -> None:
-    """Displays intersection for input math sets."""
-    if not intersection:
-        print(f'\nThere is no intersection for input math sets')
-    else:
-        print(f'\nFor input math sets the subset intersection is {intersection}\n')
-
-
-def compare_num_with_intersection(input_num: float, input_list: list) -> 'output':
-    """Checks if given number belong to intersection of initial math sets,
-    else returns the nearest range from intersection list and the nearest endpoints."""
-    in_range = False
+def compare_num_with_intersection(input_num: float, input_list: list) -> 'output data':
+    """Checks if given number belongs to intersection of initial math sets,
+    else returns the nearest range(s) from intersection list and the nearest endpoint(s)."""
     for sub_range in input_list:
-        if sub_range[0] == '-inf' and input_num <= sub_range[1]:
-            in_range = True
-            return True, sub_range, None, None
-        elif sub_range[1] == '+inf' and input_num >= sub_range[0]:
-            in_range = True
-            return True, sub_range, None, None
-        elif not isinstance(sub_range[0], str) and not isinstance(sub_range[1], str)\
-                and input_num >= sub_range[0] and input_num <= sub_range[1]:
-            # 'if B >= A and B <= C' is faster than 'if A <= B <= C'
-            in_range = True
+        if sub_range[0] == '-inf':
+            if input_num <= sub_range[1]:
+                return True, sub_range, None, None
+        elif sub_range[1] == '+inf':
+            if input_num >= sub_range[0]:
+                return True, sub_range, None, None
+        elif input_num >= sub_range[0] and input_num <= sub_range[1]:
             return True, sub_range, None, None
 
-    if not in_range:
-        ini_num_endpoints, ini_num_range = clc_diff(input_num, input_list)
-        return False, None, ini_num_endpoints, ini_num_range
+    ini_num_endpoints, ini_num_range = clc_diff(input_num, input_list)
+    return False, None, ini_num_endpoints, ini_num_range
 
 
 def clc_diff(input_num: float, input_list: list) -> 'two lists':
@@ -175,36 +110,46 @@ def clc_diff(input_num: float, input_list: list) -> 'two lists':
     return nearest_endpoints, nearest_ranges
 
 
-def main():
-    """This script gets math sets and some number and outputs the intersection of the given sets
+def main(ini_sub_sets: list, initial_num: float or int) -> None:
+    """This script gets math sets and some number,
+    then the intersection of the given math sets is defined.
     and also checks if given number belongs to subrange of the intersection.
     The first step is to get all endpoints from given math sets and form sub ranges to determine the intersection.
-    The second step is to check all ranges from minimum value to the maximum for belonging to all given subsets.
+    The second step is to check all sub ranges from minimum value to the maximum for belonging to all given subsets.
     The third step is to check the ranges from minus infinity to the minimum value
-    and from maximum value to the plus infinity for belonging to all given subsets.
+    and from maximum value to the plus infinity for belonging to all initial math subsets.
     And the forth step is to check if given number belongs to the intersection of the initial math sets,
     otherwise the nearest range(s) from the intersection and the nearest endpoint(s) are returned."""
 
-    initial_sets, initial_num = get_ini_data()
-    print(f'input data is:\n\tinitial_sets\n{initial_sets}\n\tinitial_num = {initial_num}\n')
-    all_endpoints, all_ini_sets = format_sets(initial_sets)
-    print(f'>> all endpoints are ({len(all_endpoints)}): {sorted(all_endpoints)}\n'
-          f'>> edited initial math set looks like:\n{all_ini_sets}\n')
-    intersection_set = get_ranges_for_check(all_endpoints)
-    print(f'>> endpoints converted to ranges ({len(intersection_set)}): {intersection_set}\n')
-    for set_range in all_ini_sets.values():
-        intersection_set = check_ranges(set_range, intersection_set)
-        print(f'all_ranges_list ({len(intersection_set)}):\t{intersection_set}')
-    intersection_set = common_inf_range(initial_sets, intersection_set, min(all_endpoints), max(all_endpoints))
-    print_result(intersection_set)
-    status, num_in_range, num_endpoints, num_range = compare_num_with_intersection(initial_num, intersection_set)
-    if status:
-        print(f'TASK_1, TASK_2: The given number "{initial_num}" is in intersection range: {num_in_range}')
+    all_ini_endpoints, all_ini_sets = format_sets(ini_sub_sets)
+    sub_ranges_to_check = get_ranges_for_check(all_ini_endpoints)
+    for subset in all_ini_sets:
+        sub_ranges_to_check = check_ranges(subset, sub_ranges_to_check)
+    intersection_set = common_inf_range(ini_sub_sets, sub_ranges_to_check,
+                                        min(all_ini_endpoints), max(all_ini_endpoints))
+    in_range, num_in_range, nearest_num_endpoints, nearest_num_range = compare_num_with_intersection(initial_num,
+                                                                                                     intersection_set)
+    if in_range:
+        print(f'\tTASK_1, TASK_2: The given number "{initial_num}" is in intersection range: {num_in_range}')
     else:
-        print(f'TASK_1: The given number "{initial_num}" is out of intersection range,\n'
-              f'the nearest endpoint(s) is(are): {num_endpoints}\n'
-              f'TASK_2: The given number {initial_num} is out of intersection range, the nearest is(are): {num_range}')
+        print(f'\tTASK_1: The given number "{initial_num}" is out of intersection range,\n'
+              f'\tthe nearest endpoint(s) is(are): {nearest_num_endpoints}\n\n'
+              f'\tTASK_2: The given number "{initial_num}" is out of intersection range,\n'
+              f'\tthe nearest is(are): {nearest_num_range}')
 
 
 if __name__ == '__main__':
-    main()
+    all_math_sets = {
+        'set_type_1': [['-inf', '+inf']],
+        'set_type_2': [['-inf', -10], [10, '+inf']],
+        'set_type_3': [['-inf', 99]],
+        'set_type_4': [[-98, '+inf']],
+        'set_type_5': [['-inf', -32], [-17, 22]],
+        'set_type_6': [['-inf', -41], [-18, 24], [51, 62], [103, '+inf']],
+        'set_type_7': [[-89, -61], [-24, '+inf']],
+        'set_type_8': [[-77, 61]],
+        'set_type_9': [[-89, -61], [-43, -12], [10, 27], [61, 72]]
+    }
+    for cnt, input_number in enumerate([-83, -75, -39, -14, -1, 18, 31], 1):
+        print(f'\nCase {cnt}: all types of math sets are given, number is equal to {input_number}\n')
+        main(list(all_math_sets.values()), input_number)
